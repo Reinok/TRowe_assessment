da58b921d456cd3f0eee855340e1f60eecf4bc58
Reinok <42870971+Reinok@users.noreply.github.com>
2019-12-30 16:08:12 -0500

    Update for git

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # TRowe_assessment
+
+Assumptions:
+I assumed that any metacharacters and/or punction would not be counted as part of the length of any given word.
+I assumed that users would pass in a string that was not empty to the method. This is type-hinted.
+I assumed unit test data was not being passed from file. 
+
+Test Execution Instructions:
+1. Make sure pytest is installed on the machine
+2. In a terminal, navigate to the top directory of the repository
+3. Run "pytest --junitxml=results.xml sentence_parser.py"
+    Note: the step above assumes pytest is installed in the repository, or that a virtual environment is activated that contains it in its site-packages directory. 
+4. Examine the results.xml file created in the cwd for the test results
--- sentence_parser.py
+++ sentence_parser.py
@@ -1,6 +1,7 @@
 import re
 import pytest
 
+
 def find_longest_word(sentence: str):
     without_punctuation = re.sub("[-!\.\?'\"]", "", sentence)
     words = re.split(" ", without_punctuation)
@@ -11,6 +12,7 @@ def find_longest_word(sentence: str):
             longest[0] = word
             longest[1] = word_len
     return longest
+
 
 def find_shortest_word(sentence: str):
     without_punctuation = re.sub("[-!\.\?'\"]", "", sentence)
@@ -24,14 +26,21 @@ def find_shortest_word(sentence: str):
     return shortest
 
 
-#Unit tests below, run sentense_parser.py with pytest to execute
+# Unit tests below, run sentense_parser.py with pytest to execute
 # In Terminal: pytest --junitxml=results.xml sentence_parser.py
-long_data = [("A sentence.", ["sentence", 8]), ("A super-sentence.", ["supersentence", 13])]
-short_data = [("Some short sentence.", ["Some", 4]), ("Another short sentence.", ["short", 5])]
-@pytest.mark.parametrize("test_string, expected", long_data)
-def test_longest_word_parser(test_string, expected):
-    assert find_longest_word(test_string)
+long_data = [("A sentence.", "sentence", 8), ("A super-sentence.", "supersentence", 13)]
+short_data = [("Some short sentence.", "Some", 4), ("Another short sentence.", "short", 5)]
 
-@pytest.mark.parametrize("test_string, expected", short_data)
-def test_shortest_word_parser(test_string, expected):
-    find_shortest_word(test_string)
+
+@pytest.mark.parametrize("test_string, word, length", long_data)
+def test_longest_word_parser(test_string, word, length):
+    results = find_longest_word(test_string)
+    assert (results[0] == word and
+            results[1] == length), "Expected output, {} {}, not returned.".format(word, length)
+
+
+@pytest.mark.parametrize("test_string, word, length", short_data)
+def test_shortest_word_parser(test_string, word, length):
+    results = find_shortest_word(test_string)
+    assert (results[0] == word and
+            results[1] == length), "Expected output, {} {}, not returned.".format(word, length)
